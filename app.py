d5c324bb058d567673777d61b829696a7a229e1a
Yama <gabriel.yamashita01@gmail.com>
2023-03-20 10:31:10 -0300

    adding conversation flow

--- app.py
+++ app.py
@@ -1,56 +1,65 @@
-import random
+
 from datetime import datetime
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from msgAnswer import *
 
+# Fluxo de Conversa
+from ConversationYabot import ConversationFlow
+
+
 app = Flask(__name__)
 
-
+# Home Page
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello World!"
 
 
+# Yabot Wpp
+user="Yama"
+flow = ConversationFlow()
 @app.route("/yabot", methods=['POST'])
 def sms_reply():
-    msg = request.form.get('Body')
+    # Entrada
+    incomingMessage = request.form.get('Body').lower()
+
+    # Saída
+    response = flow.processInput(incomingMessage)
+
+    # Processamento de Envio de Mensagem
     resp = MessagingResponse()
-    user="Yama"
-
-    horario = 0
-    addFlag = False
-
-    if msg.lower() in ["oie","oi", "ola", "alo", "alou", "eae", "salve"]:
-        resp.message(f"Oi, {user}")
-        resp.message(random.choice([f"Oi {user}!", "Oiee", f"Salve {user}!", f"Eae {user}, tudo bem?"]))
-
-    if msg.lower():
-        user = msg.lower()
-        resp.message(f"Hello {user}")
+    msg = resp.message()
+    msg.body(response)
 
     return str(resp)
 
 
-
+# Good Boy
 GOOD_BOY_URL = (
     "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
     "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
 )
+
 @app.route("/goodboy", methods=["GET", "POST"])
 def reply_whatsapp():
-
     try:
         num_media = int(request.values.get("NumMedia"))
+
     except (ValueError, TypeError):
         return "Invalid request: invalid or missing NumMedia parameter", 400
+
     response = MessagingResponse()
+
     if not num_media:
         msg = response.message("Send us an image!")
     else:
         msg = response.message("Thanks for the image. Here's one for you!")
         msg.media(GOOD_BOY_URL)
+
     return str(response)
 
+
+# Loop Principal
 if __name__ == "__main__":
     app.run(debug=True)
--- msgAnswer.py
+++ msgAnswer.py
@@ -1,2 +1,130 @@
 
+import json
+import uuid
 
+
+def show_json():
+   with open('messages.json', encoding='utf-8') as f:
+      data = json.load(f)
+
+   print(data)
+
+
+def write_json(dia_mes, dias_num, horario, message, filename='messages.json'):
+   new_data = {
+      "dia_mes": None,
+      "dias_num": None,
+      "horario": None,
+      "message": None
+   }
+
+   # dia_mes = input("Dias do Mês: ")
+   # dia_mes = transforma_input(dia_mes)
+   # dias_num = input("Dias da Semana: ")
+   # dias_num = transforma_input(dias_num)
+   # horario = input("Horário da Mensagem: ")
+   # message = input("Mensagem: ")
+
+   lista = [dia_mes, dias_num, horario, message]
+
+   for i, (k, v) in enumerate(new_data.items()):
+      new_data[k] = lista[i]
+
+   with open(filename,'r+') as file:
+      file_data = json.load(file)
+
+      # new_data["id"] = file_data["YABOT"][len(file_data["YABOT"]) - 1]["id"] + 1 if len(file_data["YABOT"]) >= 1 else 1
+      new_data["id"] = str(uuid.uuid4())
+      file_data["YABOT"].append(new_data)
+      file.seek(0)
+      json.dump(file_data, file, indent = 3)
+
+   print("New data has been added. | write_json()")
+
+
+def remove_json(id_, filename='messages.json'):
+   with open(filename, 'r+') as file:
+      file_data = json.load(file)
+
+   for i in file_data["YABOT"]:
+      if id_ == i['id']:
+         pos = file_data["YABOT"].index(i)
+         file_data["YABOT"].pop(pos)
+         with open(filename, 'w') as file:
+            json.dump(file_data, file, indent=3)
+
+         print(f"ID ({id_}) has been removed. | remove_json()")
+         break   
+
+   else:
+      print(f"ID ({id_}) does not exist! | remove_json()")
+
+
+def resumo_json(filename='messages.json'):
+   with open(filename, 'r') as file:
+      data = json.load(file)
+   
+   resumo = "Resumo das Mensages: "
+   for data in data["YABOT"]:
+      resumo += f"@#! ID {data['id']}) {data['message']} ({data['horario']})"
+
+   resumo = resumo.replace('@#! ', '\n')
+
+   print(f"{resumo} | resumo_json()")
+
+
+def transforma_input(input_):
+   data = input_.split()
+   input_transformado = list(map(int, data))
+
+   if len(input_transformado) == 0:
+      return None
+   else:
+      return input_transformado
+
+
+def sendMessage(message):
+   print(message)
+
+
+estado = 0
+while True:
+   wpp = input("> ")
+
+   if wpp == 'ADD':
+      estado = 'add0'
+
+      while estado != 0:               
+         if estado == 'add0':
+            dias = input("Entre os dias da mensagem ou o mês. ")
+            dias = dias.lower()
+
+            if "dia" in dias:
+               dias = dias.replace("dias ", "").strip()
+
+               dia_mes = transforma_input("")
+               dias_num = transforma_input(dias)
+
+            if "mes" in dias or "mês" in dias:
+               dias = dias.replace("mes ", "").strip()
+
+               dia_mes = transforma_input(dias)
+               dias_num = transforma_input("")
+
+            estado = 'add1'
+
+         if estado == 'add1':
+            horario = input("Entre o horário de envio da mensagem [XX:XX]. ")
+
+            estado = 'add2'
+
+         if estado == 'add2':
+            message = input("Entre a mensagem para ser enviada. ")
+
+            estado = 'add3'
+
+         if estado == 'add3':
+            write_json(dia_mes, dias_num, horario, message)
+            print("EVENTO ADICIONADO")
+
+            estado = 0
