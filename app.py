25763dbb065edbf0e152ca83dd4b7a499b8d6cd3
Yama <gabriel.yamashita01@gmail.com>
2023-04-03 01:29:29 -0300

    image recognition fixed/completed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 from email import message
 from flask import Flask, request
-# from flask_ngrok2 import run_with_ngrok
 from twilio.twiml.messaging_response import MessagingResponse
 
 # Fluxo de Conversa
@@ -9,13 +8,10 @@ from ConversationYabot import ConversationFlow
 
 
 app = Flask(__name__)
+
+# from flask_ngrok2 import run_with_ngrok
 # run_with_ngrok(app=app, auth_token='27sah00EdAmv9RxS5teUE849TnL_6TnpnpdEeozsPMpugjVvu')
 
-
-GOOD_BOY_URL = (
-    "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
-    "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-)
 
 # Home Page
 @app.route("/")
@@ -24,6 +20,11 @@ def home():
 
 
 # Yabot Wpp
+GOOD_BOY_URL = (
+    "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
+    "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+)
+
 flow = ConversationFlow()
 
 @app.route("/yabot", methods=['GET', 'POST'])
@@ -60,31 +61,6 @@ def sms_reply():
         msg.media(GOOD_BOY_URL)
 
     return str(resp)
-
-
-# Good Boy
-# GOOD_BOY_URL = (
-#     "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
-#     "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-# )
-
-@app.route("/goodboy", methods=["GET", "POST"])
-def reply_whatsapp():
-    try:
-        num_media = int(request.values.get("NumMedia"))
-
-    except (ValueError, TypeError):
-        return "Invalid request: invalid or missing NumMedia parameter", 400
-
-    response = MessagingResponse()
-
-    if not num_media:
-        msg = response.message("Send us an image!")
-    else:
-        msg = response.message("Thanks for the image. Here's one for you!")
-        msg.media(GOOD_BOY_URL)
-
-    return str(response)
 
 
 # Loop Principal
